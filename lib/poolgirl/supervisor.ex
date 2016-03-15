900e7c8aac56d10ec3e287b46efbcb724651a3a1
Cao Jiafeng <funfriendcjf@gmail.com>
2016-03-15 22:53:34 +0800

    more specific about the worker opts

--- lib/poolgirl/supervisor.ex
+++ lib/poolgirl/supervisor.ex
@@ -5,8 +5,11 @@ defmodule Poolgirl.Supervisor do
 
   def init({mod, args}) do
     options = [
+      id: mod,
+      function: :start_link,
       restart: :temporary,
-      shutdown: 5000
+      shutdown: 5000,
+      modules: [mod]
     ]
     worker = Supervisor.Spec.worker(mod, [args], options)
     Supervisor.Spec.supervise([worker], strategy: :simple_one_for_one, max_restarts: 0, max_seconds: 1)
